a6e4040550cc4e4b849e201663715fba1f271cb2
soopeach <hsjeon584@gmail.com>
2022-09-10 16:39:38 +0900

    Fix: 테스트 코드 수정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,3 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println("이거 혹시 출력이 가능하신가요?")
 }

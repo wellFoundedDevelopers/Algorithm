402fd5c0d88999aa5f49f0f384985d5468728ed0
bngsh <hee132@korea.ac.kr>
2022-09-16 21:52:23 +0900

    solved/ 카펫

---
+++ src/main/kotlin/byeonghee/1week/카펫.kt
@@ -0,0 +1,31 @@
+package byeonghee.`1week`
+
+class Solution_byeonghee {
+    fun solution(brown: Int, yellow: Int): IntArray {
+        val whSum = (brown + 4) / 2
+        for(h in 3..whSum/2) {
+            val w = whSum - h
+
+            if ((w-2) * (h-2) == yellow) {
+                return intArrayOf(w, h)
+            }
+        }
+
+        return intArrayOf()
+    }
+}
+
+fun main() {
+    val browns = arrayOf(10, 8, 24)
+    val yellows = arrayOf(2, 1, 24)
+    val ans = arrayOf(intArrayOf(4, 3), intArrayOf(3, 3), intArrayOf(8, 6))
+
+    val sol = Solution_byeonghee()
+
+    for(i in ans.indices) {
+        println(
+            if (sol.solution(browns[i], yellows[i]).contentEquals(ans[i])) "O"
+            else "X"
+        )
+    }
+}

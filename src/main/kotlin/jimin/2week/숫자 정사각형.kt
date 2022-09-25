e041f1839d7a7ee2a0a6af4ba57bf96fa6f13fad
jeeminimini <jiminn821@naver.com>
2022-09-25 16:30:02 +0900

    Add: 숫자 정사각형 구현

--- src/main/kotlin/jimin/2week/숫자 정사각형.kt
+++ src/main/kotlin/jimin/2week/숫자 정사각형.kt
@@ -1,16 +1,45 @@
 package jimin.`2week`
 
+/*
+<문제>
+https://www.acmicpc.net/problem/1051
+
+<구현 방법>
+가로, 세로 중 더 작은 것을 rectMax로 두고, 길이가 2부터 rectMax까지 사각형을 만들며 같은 수가 있는지 찾았다.
+
+<트러블 슈팅>
+처음에는 숫자들 중에 4개 이상인 것만 추출해서 그걸 기반으로 찾았다.
+그리고 row를 먼저 돌고 거기서 추출한 숫자가 있다면 이동한 영역만큼 column을 이동하는 식으로 진행했었다.
+ */
+
 import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
-    val N = readln().split(" ").first().toInt()
+    val num = readln().split(" ").map { it.toInt() }
     val rectList = arrayListOf<List<Int>>()
-    for (i in 0 until N) {
+    for (i in 0 until num[0]) {
         rectList.add(readln().chunked(1).map { it.toInt() })
     }
+    val rectMax = min(num[0], num[1])
+    var maxSize = 1
+    for (i in 0 until num[0]) {
+        for (j in 0 until num[1]) {
+            for (k in 1 until rectMax) {
+                if (j + k < num[1] && rectList[i][j] == rectList[i][j + k]) {
+                    if (i + k < num[0] && rectList[i][j] == rectList[i + k][j]) {
+                        if (rectList[i][j] == rectList[i + k][j + k]) {
+                            maxSize = max(maxSize, (k + 1) * (k + 1))
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    println(getRectMaxSize(rectList))
+    println(maxSize)
 }
+/*
 
 fun getRectMaxSize(rectList: ArrayList<List<Int>>): Int {
     val rectSet = rectList.flatten().toSet()
@@ -40,3 +69,4 @@ fun getRectMaxSize(rectList: ArrayList<List<Int>>): Int {
     }
     return max
 }
+*/

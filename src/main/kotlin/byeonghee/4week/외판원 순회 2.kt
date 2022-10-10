10dba00d1d89eaed093c220e3c97081e2018d98f
bngsh <hee132@korea.ac.kr>
2022-10-10 14:00:11 +0900

    solve2: 외판원 순회 2

--- src/main/kotlin/byeonghee/4week/외판원 순회 2.kt
+++ src/main/kotlin/byeonghee/4week/외판원 순회 2.kt
@@ -15,20 +15,9 @@ fun getBit(city: Int) : Int {
     return 1 shl city
 }
 
-fun getOnes(bits: Int) : Int {
-    var num = bits
-    var ans = 0
-    while(num > 0) {
-        ans += num and 1
-        num /= 2
-    }
-    return ans
-}
-
-fun getBeforeList(route: Int, first: Int) : List<Travel> {
+fun getBeforeList(route: Int) : List<Travel> {
     val list = mutableListOf<Travel>()
-    for(i in 0 until n) {
-        if (i == first) continue
+    for(i in 1 until n) {
         getBit(i).run {
             if ((route and this) != 0) {
                 list.add(Travel(i, route xor this))
@@ -38,15 +27,15 @@ fun getBeforeList(route: Int, first: Int) : List<Travel> {
     return list
 }
 
-fun dfs(route: Int, first: Int, last: Int) : Int {
-    if (getOnes(route) == 1) return adjacency[first][last]
+fun dfs(route: Int, last: Int) : Int {
+    if (route == 0) return adjacency[0][last]
     if (dp[route][last] > -1) return dp[route][last]
 
     var minLocalCost = Int.MAX_VALUE
 
-    getBeforeList(route, first).forEach { before ->
+    getBeforeList(route).forEach { before ->
         if (dp[before.route][before.city] == -1) {
-            dp[before.route][before.city] = dfs(before.route, first, before.city)
+            dp[before.route][before.city] = dfs(before.route, before.city)
         }
 
         if (dp[before.route][before.city] > 0 && adjacency[before.city][last] > 0) {
@@ -57,9 +46,7 @@ fun dfs(route: Int, first: Int, last: Int) : Int {
         }
     }
 
-    minLocalCost = minLocalCost.let { if (it == Int.MAX_VALUE) 0 else it }
-    println("START ${getBit(first).toString(2).padStart(n, '0').reversed()} MOVE ${route.toString(2).padStart(n, '0').reversed()} TO ${(route or getBit(last)).toString(2).padStart(n, '0').reversed()}" +
-            " COST $minLocalCost")
+    minLocalCost = minLocalCost.let{ if (it == Int.MAX_VALUE) 0 else it }
     return minLocalCost
 }
 
@@ -69,13 +56,9 @@ fun main() {
 
     adjacency = Array(n) { br.readLine().split(" ").map{ it.toInt() }.toIntArray() }
 
-    var answer = Int.MAX_VALUE
     for(i in 0 until n) {
-        dp[complete][i] = dfs(complete, i, i)
-        answer = if (dp[complete][i] == 0) answer else Integer.min(answer, dp[complete][i])
-        dp.forEach { it.fill(-1) }
+        dp[0][i] = adjacency[0][i]
     }
-    answer = answer.let { if (it == Int.MAX_VALUE) 0 else it }
 
-    println(answer)
+    println(dfs(complete - 1, 0))
 }

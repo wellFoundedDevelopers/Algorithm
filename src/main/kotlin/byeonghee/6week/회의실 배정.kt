376afb4a0921695f2a355373db74d12af7e805a4
bngsh <hee132@korea.ac.kr>
2022-10-22 19:25:10 +0900

    chore: 전역변수 숨김

--- src/main/kotlin/byeonghee/6week/회의실 배정.kt
+++ src/main/kotlin/byeonghee/6week/회의실 배정.kt
@@ -3,31 +3,45 @@ package byeonghee.`6week`
 import java.io.*
 import java.util.PriorityQueue
 
-val br = BufferedReader(InputStreamReader(System.`in`))
+class `소병희_회의실 배정` {
+    companion object {
+        fun getSolution(): Solution {
+            return Solution()
+        }
+    }
 
-data class Meeting(val s: Int, val e: Int)
+    class Solution {
+        val br = BufferedReader(InputStreamReader(System.`in`))
 
-var n = 0
-val meetings = PriorityQueue(Comparator<Meeting> { a, b -> if (a.e == b.e) a.s - b.s else a.e - b.e })
-var answer = 0
-var curLastT = 0
-lateinit var nxt : Meeting
+        data class Meeting(val s: Int, val e: Int)
+
+        var n = 0
+        val meetings = PriorityQueue(Comparator<Meeting> { a, b -> if (a.e == b.e) a.s - b.s else a.e - b.e })
+        var answer = 0
+        var curLastT = 0
+        lateinit var nxt : Meeting
+
+        fun solution() {
+            n = br.readLine().toInt()
+            repeat(n) {
+                br.readLine().split(" ").map{ it.toInt() }.run{
+                    meetings.add(Meeting(first(), last()))
+                }
+            }
+
+            while(meetings.isNotEmpty()) {
+                nxt = meetings.poll()
+                if (curLastT <= nxt.s) {
+                    curLastT = nxt.e
+                    answer++
+                }
+            }
+
+            println(answer)
+        }
+    }
+}
 
 fun main() {
-    n = br.readLine().toInt()
-    repeat(n) {
-        br.readLine().split(" ").map{ it.toInt() }.run{
-            meetings.add(Meeting(first(), last()))
-        }
-    }
-
-    while(meetings.isNotEmpty()) {
-        nxt = meetings.poll()
-        if (curLastT <= nxt.s) {
-            curLastT = nxt.e
-            answer++
-        }
-    }
-
-    println(answer)
+    `소병희_회의실 배정`.getSolution()
 }

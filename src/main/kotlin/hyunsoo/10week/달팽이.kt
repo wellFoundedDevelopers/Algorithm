0fe2536777d670a9cf8233b8483d2b143e3d0abd
soopeach <hsjeon584@gmail.com>
2022-11-20 00:53:52 +0900

    Feat: 달팽이 추가

---
+++ src/main/kotlin/hyunsoo/10week/달팽이.kt
@@ -0,0 +1,60 @@
+package hyunsoo.`10week`
+
+// 하 우 좌 상
+val dirList = listOf(
+    Pair(1, 0),
+    Pair(0, 1),
+    Pair(0, -1),
+    Pair(-1, 0),
+)
+
+val n = readln().toInt()
+val target = readln().toInt()
+var num = n * n
+
+val snail = Array(n) {
+    IntArray(n)
+}
+
+fun main() {
+
+    var x = 0
+    var y = 0
+
+    var curDir = 0
+    snail[x][y] = num--
+
+    while (num >= 1) {
+
+        val (dx, dy) = dirList[curDir]
+        while (
+            x + dx in 0 until n &&
+            y + dy in 0 until n &&
+            snail[x + dx][y + dy] == 0
+        ) {
+            x += dx
+            y += dy
+            snail[x][y] = num--
+            if (num == 0) showSnailAndGetTargetCoordinate()
+        }
+        curDir = (curDir + 1) % 4
+    }
+
+}
+
+fun showSnailAndGetTargetCoordinate() {
+    snail.map {
+        println(it.joinToString(" "))
+    }
+
+    snail.forEachIndexed { index, array ->
+        array.indexOf(target).apply {
+            if (this != -1) {
+                println("${index + 1} ${this + 1}")
+            }
+        }
+    }
+
+    System.exit(0)
+
+}

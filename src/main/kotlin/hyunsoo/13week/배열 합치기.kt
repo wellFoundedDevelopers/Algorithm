c7fe249ed7d7ee2eaa7574ad891a4c0395d34506
soopeach <hsjeon584@gmail.com>
2023-01-01 01:04:03 +0900

    Feat: 배열 합치기

---
+++ src/main/kotlin/hyunsoo/13week/배열 합치기.kt
@@ -0,0 +1,46 @@
+package hyunsoo.`13week`
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (aSize, bSize) = br.readLine().split(" ").map { it.toInt() }
+    val arrayA = br.readLine().split(" ").map { it.toInt() }
+    val arrayB = br.readLine().split(" ").map { it.toInt() }
+    val sortedArray = IntArray(aSize + bSize)
+
+    var aPointer = 0
+    var bPointer = 0
+    var sortedPointer = 0
+
+    while (aPointer <= arrayA.lastIndex && bPointer <= arrayB.lastIndex) {
+
+        if (arrayA[aPointer] < arrayB[bPointer]) {
+            sortedArray[sortedPointer++] = arrayA[aPointer++]
+        } else {
+            sortedArray[sortedPointer++] = arrayB[bPointer++]
+        }
+    }
+
+    while (aPointer <= arrayA.lastIndex) {
+        sortedArray[sortedPointer++] = arrayA[aPointer++]
+    }
+
+    while (bPointer <= arrayB.lastIndex) {
+        sortedArray[sortedPointer++] = arrayB[bPointer++]
+    }
+
+
+    sortedArray.forEach {
+        bw.write("$it ")
+    }
+
+    bw.flush()
+    bw.close()
+}

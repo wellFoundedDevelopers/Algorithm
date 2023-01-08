ae66763df7fc2646b3fa1fad9dba2bbcb235d445
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-01-08 21:12:12 +0900

    solve: 대표 자연수

---
+++ src/main/kotlin/heejik/14week/대표 자연수.kt
@@ -0,0 +1,36 @@
+package heejik.`14week`
+
+import kotlin.math.absoluteValue
+
+class `대표 자연수` {
+
+    var minScore = Int.MAX_VALUE
+    var center = -1
+    lateinit var nums: List<Int>
+    fun solve() {
+        val n = readln().toInt()
+        nums = readln().split(' ').map { it.toInt() }.sorted()
+
+        for (i in nums.first()..nums.last()) {
+            getScore(i)
+        }
+
+        println(center)
+    }
+
+    private fun getScore(num: Int) {
+        var tmp = 0
+        nums.forEach {
+            tmp += (num - it).absoluteValue
+        }
+
+        if (tmp < minScore) {
+            minScore = tmp
+            center = num
+        }
+    }
+}
+
+fun main() {
+    `대표 자연수`().solve()
+}

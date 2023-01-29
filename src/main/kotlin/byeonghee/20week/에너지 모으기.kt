b2937679face082f27c1705b73752d330a9a439a
bngsh <hee132@korea.ac.kr>
2023-01-29 22:56:32 +0900

    solve: 에너지 모으기

--- src/main/kotlin/byeonghee/20week/에너지 모으기.kt
+++ src/main/kotlin/byeonghee/20week/에너지 모으기.kt
@@ -1,47 +1,36 @@
 package byeonghee.`20week`
 
 import java.io.*
-import java.util.PriorityQueue
-import kotlin.math.max
 
-data class Node(var v: Int, var pre: Node? = null, var nxt: Node? = null)
-data class Pick(val maxNode: Node, val rm: Node)
+class 소병희_에너지모으기 {
 
-fun main() : Unit = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val n = readLine().toInt()
+    companion object {
+        fun solve(): Unit = with(BufferedReader(InputStreamReader(System.`in`))) {
+            val n = readLine().toInt()
+            val beads = readLine().split(" ").map { it.toInt() }
+            var answer = 0
 
-    val balls = readLine().split(" ").map{ Node(it.toInt()) }
-    val maxV = balls.maxOf { it.v }
-    val maxList = balls.filter { it.v == maxV }
-    for(i in 0 until n - 1) {
-        balls[i].nxt = balls[i + 1]
-        balls[i + 1].pre = balls[i]
+            for(i in 1 until n - 1) {
+                answer = answer.coerceAtLeast(dfs(beads, i))
+            }
+
+            println(answer)
+        }
+
+        fun dfs(list: List<Int>, rm: Int) : Int {
+            val preSum = list[rm - 1] * list[rm + 1]
+            val newList = list.toMutableList().apply { removeAt(rm) }
+            var ret = 0
+
+            for(i in 1 until newList.lastIndex) {
+                ret = ret.coerceAtLeast(dfs(newList, i))
+            }
+
+            return preSum + ret
+        }
     }
+}
 
-    val pq = PriorityQueue(Comparator<Pick> { a, b -> b.rm.v - a.rm.v })
-    var answer = 0
-
-    for(i in maxList.indices) {
-        if (maxList[i].pre != null) pq.add(Pick(maxList[i], maxList[i].pre!!))
-        if (maxList[i].nxt != null) pq.add(Pick(maxList[i], maxList[i].nxt!!))
-    }
-
-    while(pq.isNotEmpty()) {
-        val (maxNode, rm) = pq.poll()
-        if (rm.pre == null || rm.nxt == null) continue
-
-        answer += rm.pre!!.v * rm.nxt!!.v
-
-        rm.pre?.nxt = rm.nxt
-        rm.nxt?.pre = rm.pre
-        rm.pre = null
-        rm.nxt = null
-
-        if (maxNode.pre != null) pq.add(Pick(maxNode, maxNode.pre!!))
-        if (maxNode.nxt != null) pq.add(Pick(maxNode, maxNode.nxt!!))
-    }
-
-    answer += balls.first().v * balls.last().v
-    println(answer)
-
+fun main() {
+    소병희_에너지모으기.solve()
 }

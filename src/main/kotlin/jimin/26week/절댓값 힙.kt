ea2b27a88eb5accffe2d26146df292544d5456da
jimin <jiminn821@naver.com>
2023-03-09 04:10:29 +0900

    feat : 절댓값 힙 구현

---
+++ src/main/kotlin/jimin/26week/절댓값 힙.kt
@@ -0,0 +1,40 @@
+package jimin.`26week`
+
+/*
+<문제>
+[절댓값 ](https://www.acmicpc.net/problem/11286)
+
+<구현 방법>
+입력 받을 때 0이 아니면 heap에 추가해준다.
+이때 Pair로 추가해줘서 첫번째값은 절댓값, 두번째값은 원래 값으로 하여 이 순서로 우선순위가 되도록 하였다.
+
+<트러블 슈팅>
+*/
+
+import java.util.*
+class `절댓값 힙` {
+    fun solve() {
+        val heap = PriorityQueue(Comparator<Pair<Int, Int>>{ a, b -> if (a.first != b.first) a.first - b.first else a.second - b.second})
+
+        repeat(readln().toInt()) {
+            readln().toInt().apply{
+                if (this == 0) {
+                    if (heap.isNotEmpty()) {
+                        println(heap.remove().second)
+                    } else {
+                        println(0)
+                    }
+
+                } else if (this < 0) {
+                    heap.add(Pair(this * (-1), this))
+                } else {
+                    heap.add(Pair(this, this))
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    `절댓값 힙`().solve()
+}

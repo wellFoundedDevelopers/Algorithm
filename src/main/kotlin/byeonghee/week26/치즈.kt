4b5dbde976e4ac0fb85833edcb9ee324ca6434a1
bngsh <hee132@korea.ac.kr>
2023-03-09 14:23:19 +0900

    chore: 템플릿 적용

--- src/main/kotlin/byeonghee/week26/치즈.kt
+++ src/main/kotlin/byeonghee/week26/치즈.kt
@@ -2,66 +2,75 @@ package byeonghee.week26
 
 import java.io.StreamTokenizer
 
-const val AIR = 0
-const val CHEESE = 1
+class 소병희_치즈 {
 
-val dr = arrayOf(1, 0, -1, 0)
-val dc = arrayOf(0, 1, 0, -1)
+    companion object {
+        const val AIR = 0
+        const val CHEESE = 1
 
-fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
+        val dr = arrayOf(1, 0, -1, 0)
+        val dc = arrayOf(0, 1, 0, -1)
 
-    fun input() : Int {
-        nextToken()
-        return nval.toInt()
-    }
+        fun solve() = StreamTokenizer(System.`in`.bufferedReader()).run {
 
-    val h = input()
-    val w = input()
-    val plane = Array(h) { IntArray(w) }
-
-    val q = ArrayList<Pair<Int, Int>>()
-    var cheese = 0
-    var record = 0
-    var timeCost = 0
-
-    for(r in 0 until h) for(c in 0 until w) {
-        plane[r][c] = input()
-        if (plane[r][c] == CHEESE) cheese++
-    }
-
-    var r = 0
-    var c = 0
-    var nr = 0
-    var nc = 0
-    while(cheese > 0) {
-        val visited = Array(h) { BooleanArray(w) }
-        record = cheese
-        q.add(Pair(0, 0))
-        visited[0][0] = true
-
-        while(q.isNotEmpty()) {
-            q.removeFirst().let {
-                r = it.first
-                c = it.second
+            fun input() : Int {
+                nextToken()
+                return nval.toInt()
             }
 
-            for(d in 0 until 4) {
-                nr = r + dr[d]
-                nc = c + dc[d]
+            val h = input()
+            val w = input()
+            val plane = Array(h) { IntArray(w) }
 
-                if (nr !in 0 until h || nc !in 0 until w) continue
-                if (visited[nr][nc]) continue
+            val q = ArrayList<Pair<Int, Int>>()
+            var cheese = 0
+            var record = 0
+            var timeCost = 0
 
-                if (plane[nr][nc] == CHEESE) {
-                    plane[nr][nc] = AIR
-                    cheese--
+            for(r in 0 until h) for(c in 0 until w) {
+                plane[r][c] = input()
+                if (plane[r][c] == CHEESE) cheese++
+            }
+
+            var r = 0
+            var c = 0
+            var nr = 0
+            var nc = 0
+            while(cheese > 0) {
+                val visited = Array(h) { BooleanArray(w) }
+                record = cheese
+                q.add(Pair(0, 0))
+                visited[0][0] = true
+
+                while(q.isNotEmpty()) {
+                    q.removeFirst().let {
+                        r = it.first
+                        c = it.second
+                    }
+
+                    for(d in 0 until 4) {
+                        nr = r + dr[d]
+                        nc = c + dc[d]
+
+                        if (nr !in 0 until h || nc !in 0 until w) continue
+                        if (visited[nr][nc]) continue
+
+                        if (plane[nr][nc] == CHEESE) {
+                            plane[nr][nc] = AIR
+                            cheese--
+                        }
+                        else q.add(Pair(nr, nc))
+                        visited[nr][nc] = true
+                    }
                 }
-                else q.add(Pair(nr, nc))
-                visited[nr][nc] = true
+                timeCost++
             }
-        }
-        timeCost++
-    }
 
-    println("$timeCost\n$record")
+            println("$timeCost\n$record")
+        }
+    }
+}
+
+fun main() {
+    소병희_치즈.solve()
 }

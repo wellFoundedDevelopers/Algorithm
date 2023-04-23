ebfd8f5bff8f473f4b93f5154776ded3abc0887b
bngsh <hee132@korea.ac.kr>
2023-04-23 16:47:29 +0900

    solve: 이모티콘

---
+++ src/main/kotlin/byeonghee/week31/이모티콘.kt
@@ -0,0 +1,22 @@
+package byeonghee.week31
+
+class 소병희_ {
+
+    companion object {
+        fun solve() {
+            val s = readln().toInt()
+            val dp = IntArray(s+1) { it }.apply { this[1] = 0 }
+
+            for(clipboard in 2 until s) for(emoji in clipboard .. s) {
+                val ops = 1 + (emoji - clipboard) / clipboard + if (emoji % clipboard == 0) 0 else (1 + clipboard - (emoji % clipboard))
+                dp[emoji] = dp[emoji].coerceAtMost(dp[clipboard] + ops)
+            }
+
+            print(dp[s])
+        }
+    }
+}
+
+fun main() {
+    소병희_.solve()
+}

765b1661fc7cc0722a407e2392543e43083500da
jeeminimini <jiminn821@naver.com>
2023-04-30 03:14:38 +0900

    feat : 운동 구현

--- src/main/kotlin/jimin/30week/과제.py
+++ src/main/kotlin/jimin/30week/과제.py
@@ -26,7 +26,7 @@ dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if j <= assignments[i][0]:
-            if dp[i - 1][j - 1] + assignments[i][1] > dp[i - 1][j]:
+            if dp[i - 1][j - 1] + assignments[i][1] > dp[i - 1][j]:합
                 dp[i][j] = dp[i - 1][j - 1] + assignments[i][1]
             else:
                 dp[i][j] = dp[i - 1][j]

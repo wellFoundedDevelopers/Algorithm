770756f98758ff3c021db5e11735f03d86922875
bngsh <hee132@korea.ac.kr>
2023-05-03 15:08:51 +0900

    solve: Contact

---
+++ src/main/kotlin/byeonghee/week34/Contact.kt
@@ -0,0 +1,28 @@
+package byeonghee.week34
+
+import java.io.*
+
+class 소병희_ {
+
+    companion object {
+        fun solve(): Unit = with(BufferedReader(InputStreamReader(System.`in`))) {
+            val T = readLine().toInt()
+            val vega = "^((100+1+)|(01))+$".toRegex()
+            val sb = StringBuilder()
+
+            repeat(T) {
+                val wave = readLine()
+                when (vega.matches(wave)) {
+                    true -> sb.appendLine("YES")
+                    else -> sb.appendLine("NO")
+                }
+            }
+
+            print(sb)
+        }
+    }
+}
+
+fun main() {
+    소병희_.solve()
+}

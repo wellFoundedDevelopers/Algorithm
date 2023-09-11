730281a16474fc8f1d7b2e38cdfb46af98765312
bngsh <hee132@gmail.com>
2023-09-11 20:05:20 +0900

    fix: 클래스명 수정

--- src/main/kotlin/byeonghee/week44/함께 블록 쌓기.kt
+++ src/main/kotlin/byeonghee/week44/함께 블록 쌓기.kt
@@ -1,25 +1,34 @@
 package byeonghee.week44
 
-const val MOD = 10_007
+class 소병희_함께블록쌓기 {
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val (n, m, h) = readLine().split(" ").map { it.toInt() }
-    val dp = Array(n+1) { IntArray(h+1) }
+    companion object {
+        const val MOD = 10_007
 
-    dp[0][0] = 1
-    repeat(n) { i ->
+        fun solve() = with(System.`in`.bufferedReader()) {
+            val (n, m, h) = readLine().split(" ").map { it.toInt() }
+            val dp = Array(n+1) { IntArray(h+1) }
 
-        for(k in 0 .. h) {
-            dp[i+1][k] = dp[i][k]
-        }
+            dp[0][0] = 1
+            repeat(n) { i ->
 
-        readLine().split(" ").forEach {
-            val j = it.toInt()
-            for(k in 0 .. h - j) {
-                dp[i+1][k + j] = (dp[i+1][k + j] + dp[i][k]) % MOD
+                for(k in 0 .. h) {
+                    dp[i+1][k] = dp[i][k]
+                }
+
+                readLine().split(" ").forEach {
+                    val j = it.toInt()
+                    for(k in 0 .. h - j) {
+                        dp[i+1][k + j] = (dp[i+1][k + j] + dp[i][k]) % MOD
+                    }
+                }
             }
+
+            println(dp[n][h])
         }
     }
+}
 
-    println(dp[n][h])
+fun main() {
+    소병희_함께블록쌓기.solve()
 }

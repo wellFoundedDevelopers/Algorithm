6520a7cab8fcfc5079cdd4fa9096a96804e02a0b
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-11-12 21:01:57 +0900

    fix 백도어

--- src/main/kotlin/heejik/48week/백도어.kt
+++ src/main/kotlin/heejik/48week/백도어.kt
@@ -1,52 +1,48 @@
 package heejik.`48week`
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.util.PriorityQueue
 import kotlin.properties.Delegates
 
 class 백도어 {
 
     var n by Delegates.notNull<Int>()
     var m by Delegates.notNull<Int>()
-    private lateinit var roads: MutableList<MutableList<Road>>
+    private lateinit var roads: List<MutableList<Road>>
 
     data class Road(
         val destination: Int,
-        val time: Int
+        val time: Long
     )
 
-    fun solve() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    fun solve() = with(System.`in`.bufferedReader()) {
         readLine().split(' ').map { it.toInt() }.run {
             n = this[0]
             m = this[1]
         }
-        val cant = mutableListOf<Int>()
-        readLine().split(' ').map { it.toInt() }.forEachIndexed { index, it ->
-            if (it == 1 && index != n - 1) cant.add(index)
-        }
+        val cant = readLine().split(' ').map { it == "1" } as MutableList
+        cant[n - 1] = false
 
-        roads = MutableList(n) { mutableListOf() }
+        roads = List(n) { mutableListOf() }
         repeat(m) {
             val (a, b, t) = readLine().split(' ').map { it.toInt() }
-            if (a in cant || b in cant) return@repeat
-            roads[a].add(Road(b, t))
-            roads[b].add(Road(a, t))
+            if (cant[a] || cant[b]) return@repeat   // in 으로 해서 문제였구나,,,
+            roads[a].add(Road(b, t.toLong()))
+            roads[b].add(Road(a, t.toLong()))
         }
-
 
         println(getMinTime(0))
     }
 
 
-    fun getMinTime(start: Int): Int {
-        val times = MutableList(n) { Int.MAX_VALUE }
-        times[start] = 0
+    fun getMinTime(start: Int): Long {
+        val times = LongArray(n) { Long.MAX_VALUE }
+        times[start] = 0L
 
-        val deque = ArrayDeque<Road>()
-        deque.add(Road(start, 0))
+        val pq = PriorityQueue<Road> { a1, a2 -> (a1.time - a2.time).toInt() }
+        pq.add(Road(start, 0L))
 
-        while (deque.isNotEmpty()) {
-            val (current, time) = deque.removeFirst()
+        while (pq.isNotEmpty()) {
+            val (current, time) = pq.poll()
             if (times[current] < time) continue
 
             roads[current].forEach {
@@ -54,14 +50,15 @@ class 백도어 {
                 val newTime = time + it.time
                 if (newTime < times[destination]) {
                     times[destination] = newTime
-                    deque.add(Road(destination, newTime))
+                    pq.add(Road(destination, newTime))
                 }
             }
         }
-        return if (times[n - 1] == Int.MAX_VALUE) -1 else times[n - 1]
+        return if (times[n - 1] == Long.MAX_VALUE) -1 else times[n - 1]
     }
 }
 
 fun main() {
     백도어().solve()
 }
+
